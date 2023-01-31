9d88509417177d0d56c5629be864f1f04e47613e
Savion Watson <72318904+SavionW-Works@users.noreply.github.com>
2023-01-30 21:46:04 -0500

    Finished Assignment w/ Bonus Acceptance

--- utils/event.js
+++ utils/event.js
@@ -1,18 +1,44 @@
 import { add } from "./math";
-
+import { multi } from "./math";
 const eventHandler = () => {
 
-    const addButton = document.getElementById('add');
+    const addButton = document.getElementById('add'); 
+    const multiplyButton = document.getElementById('multiply');
     const number1 = document.getElementById('number-1');
     const number2 = document.getElementById('number-2');
     const result = document.getElementById('result');
-    const updateDomWithAddedNumbers = () => {
-        result.innerHTML = 
+    
+    const addNums = () => {
+        var ans = add(number1.value,number2.value);
+        
+        if (!ans) { //Checks if ans is NaN
+            result.innerHTML = "Error! Parameter missing or not an int.";
+        } 
+
+        else { //Valid Answer
+            result.innerHTML = 
             // parseInt(number1.value, 10)+parseInt(number2.value,10);
-            add(number1.value,number2.value);
+            "Addition = " + ans;
+        }
+    } 
+
+    const multiplyNums = () => {
+        
+        var ans = multi(number1.value,number2.value);
+        if (!ans) { //Checks if ans is NaN
+            result.innerHTML = "Error! Parameter missing or not an int.";
+        } 
+
+        else { //Valid Answer
+            result.innerHTML = 
+            // parseInt(number1.value, 10)+parseInt(number2.value,10);
+            "Multiplication = " + ans;
+        }
+        
     }
 
-    addButton.addEventListener('click', updateDomWithAddedNumbers);
+    addButton.addEventListener('click', addNums); 
+    multiplyButton.addEventListener('click', multiplyNums);
 
 }
 
